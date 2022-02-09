b6c6a55078f69de4054e898a809b2b7143879efa
akshitha7777 <akshi42133@gmail.com>
2022-02-09 12:18:55 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  userId = '' ;
+  id = '1';
   title = 'question1-angular';
+  body = 'question1-angular';
+ public data: any = []
+ constructor(public http: HttpClient) {
+  
+ }
+
+ getData(){
+   const url ='https://jsonplaceholder.typicode.com/posts'
+   this.http.get(url).subscribe((res)=>{
+     this.data = res
+     console.log(this.data)
+
+   })
+ }
+
+ ngOnInit() {
+   this.getData()
+ }
 }
